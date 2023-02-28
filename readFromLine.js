94b9830f14d12c1080adb7d0969d7977d6b418b8
Vartika Singh <vartikatomar270@gmail.com>
2023-02-28 17:51:40 +0530

    initial commit

---
+++ readFromLine.js
@@ -0,0 +1,11 @@
+// readFromLine.js
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (name) => {
+    console.log(`Hello ${name}`);
+    rl.close();
+  });
